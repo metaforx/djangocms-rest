479e43f3d5a67db30a35ed69c4ac568a8bd41097
Marc Widmer <marc@pbi.io>
2025-03-02 22:23:46 +0100

    Add PageListView with pagination
    - Implement new PageListView for retrieving paginated page lists
    - Add BaseListAPIView as a base class for list views
    - Create PageListSerializer to represent page list details
    - Update URLs to include new page-list endpoint
    - Support language-specific and permission-filtered page listings

--- djangocms_rest/views_base.py
+++ djangocms_rest/views_base.py
@@ -1,5 +1,6 @@
 from django.contrib.sites.shortcuts import get_current_site
 from django.utils.functional import cached_property
+from rest_framework.generics import ListAPIView
 from rest_framework.views import APIView
 
 
@@ -9,6 +10,18 @@ class BaseAPIView(APIView):
     """
 
     http_method_names = ("get", "options")
+
+    @cached_property
+    def site(self):
+        """
+        Fetch and cache the current site and make it available to all views.
+        """
+        return get_current_site(self.request)
+
+class BaseListAPIView(ListAPIView):
+    """
+    This is a base class for all list API views. It supports default pagination and sets the allowed methods to GET and OPTIONS.
+    """
 
     @cached_property
     def site(self):
