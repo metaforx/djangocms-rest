a33b47c311fa4f5a1cb6214069755a6429199e4e
Marc Widmer <marc@pbi.io>
2025-03-27 12:18:49 +0100

    add cms.urls for fetching urls in serializers tests

--- tests/urls.py
+++ tests/urls.py
@@ -2,11 +2,14 @@ from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import include, path
 
-
 admin.autodiscover()
 
 urlpatterns = [
-    path("api/", include("djangocms_rest.urls")),
+    path(
+        "api/",
+        include("djangocms_rest.urls"),
+    ),
+    path("", include("cms.urls")),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
