6a027f10a68e25dfcb2065f8f09047daa26fde9a
Fabian Braun <fsbraun@gmx.de>
2025-08-19 09:07:24 +0200

    fix: Import cycle

--- djangocms_rest/serializers/utils/render.py
+++ djangocms_rest/serializers/utils/render.py
@@ -1,10 +1,10 @@
-from cms.plugin_rendering import ContentRenderer
-
 from sekizai.context import SekizaiContext
 from sekizai.helpers import get_varname
 
 
 def render_html(request, placeholder, language):
+    from cms.plugin_rendering import ContentRenderer
+
     content_renderer = ContentRenderer(request)
     context = SekizaiContext({"request": request, "LANGUAGE_CODE": language})
     content = content_renderer.render_placeholder(
